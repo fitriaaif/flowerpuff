1bbaba0356df20cd94bbcb307d9e61be6615d22d
Farhanmuh69 <fmuh748@gmail.com>
2024-06-28 03:45:31 +0700

    sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://flowerpuff.vercel.app/', // Ganti dengan URL situs Anda
+  generateRobotsTxt: true,
+};
--- sitemap.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://www.yourwebsite.com', // Ganti dengan URL situs Anda
-    generateRobotsTxt: true,
-  };
-  

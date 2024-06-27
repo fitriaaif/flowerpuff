48b19fbbf0af09646a3fabc6c9b3d2359a5cf386
21.11.4263_Farhanmuh <107019382+Farhanmuh69@users.noreply.github.com>
2024-06-27 22:45:18 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,42 @@
 import Image from "next/image";
-import Navbar from "../Components/Navbar"
-import Hero from "../Components/Hero"
-import PageBreak from "../Components/PageBreak"
-import Content from "../Components/Content"
-import CardList from "../Components/CardProductList"
-import CardReviewList from "../Components/CardReviewList"
-import Location from "../Components/Location"
-import Footer from "../Components/Footer"
-import { GoogleTagManager } from '@next/third-parties/google'
+import Navbar from "../Components/Navbar";
+import Hero from "../Components/Hero";
+import PageBreak from "../Components/PageBreak";
+import Content from "../Components/Content";
+import CardList from "../Components/CardProductList";
+import CardReviewList from "../Components/CardReviewList";
+import Location from "../Components/Location";
+import Footer from "../Components/Footer";
+import Script from 'next/script';
 
 export default function Home() {
   return (
     <main>
       <div>
-        <GoogleTagManager gtmId="G-B20N8LCNY3" />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-CQFRF5Y66E`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CQFRF5Y66E', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
         <Navbar/>
         <Hero/>
         <PageBreak/>
         <Content/>
         <div className="mt-28">
-        <CardList/>
+          <CardList/>
         </div>
         <CardReviewList/>
         <Location/>

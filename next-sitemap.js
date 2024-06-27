9cda4fd771dd1e8c546c9a56da1361d53146dbf9
21.11.4263_Farhanmuh <107019382+Farhanmuh69@users.noreply.github.com>
2024-06-28 03:58:41 +0700

    Update next-sitemap.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://flowerpuff.vercel.app/', // Ganti dengan URL situs Anda
+  siteUrl: 'https://flowerpuff.vercel.app', // Ganti dengan URL situs Anda
   generateRobotsTxt: true,
 };

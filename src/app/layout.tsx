35927da9d1084984b38a14fc3238df3eda0426c0
Farhanmuh69 <fmuh748@gmail.com>
2024-07-03 11:05:54 +0700

    updaten

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,14 +13,8 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <html lang="en">
       <Head>
-      <title>Flowerpuff</title>
-        <meta name="description" content="Flowerpuff | Bucket Bunga Jogja - Menyediakan bucket bunga wisuda, bucket bunga custom, dan bucket bunga murah di Jogja." />
-        <meta name="keywords" content="bucket bunga, bucket bunga jogja, bucket bunga wisuda, bucket bunga custom, bucket bunga murah, flowerpuff" />
-        <meta property="og:title" content="Flowerpuff | Bucket Bunga Jogja" />
-        <meta property="og:description" content="Menyediakan bucket bunga wisuda, bucket bunga custom, dan bucket bunga murah di Jogja." />
-        <meta property="og:image" content="/path-to-your-image.jpg" />
-        <meta property="og:url" content="https://www.your-website.com" />
-        <meta name="twitter:card" content="summary_large_image" />
+        <title>Flowerpuff</title>
+        <meta name="flowerpuff|bucket bunga jogja" content="bucket bunga wisuda | bucket bunga custom | bucket bunga murah sejogja" />
         <link rel="icon" href="/favicon.ico" />
         {/* Google Tag Manager script */}
         <script
@@ -37,10 +31,6 @@ const Layout = ({ children }: LayoutProps) => {
         {/* End Google Tag Manager */}
       </Head>
       <body>
-        <Script>
-        localStorage.theme = 'light';
-        document.documentElement.classList.remove('dark');
-        </Script>
         {/* Google Analytics gtag.js */}
         <Script
           strategy="afterInteractive"
@@ -73,73 +63,4 @@ const Layout = ({ children }: LayoutProps) => {
     </html>
   );
 };
-
-export default Layout;
-// import './globals.css';
-// import { ReactNode } from 'react';
-// import Head from 'next/head';
-// import Script from 'next/script';
-
-// // Define interface for layout props
-// interface LayoutProps {
-//   children: ReactNode;
-// }
-
-// // Define layout component
-// const Layout = ({ children }: LayoutProps) => {
-//   return (
-//     <html lang="en">
-//       <Head>
-//         <title>Flowerpuff</title>
-//         <meta name="description" content="Flowerpuff | Bucket Bunga Jogja - Menyediakan bucket bunga wisuda, bucket bunga custom, dan bucket bunga murah di Jogja." />
-//         <meta name="keywords" content="bucket bunga, bucket bunga jogja, bucket bunga wisuda, bucket bunga custom, bucket bunga murah, flowerpuff" />
-//         <meta property="og:title" content="Flowerpuff | Bucket Bunga Jogja" />
-//         <meta property="og:description" content="Menyediakan bucket bunga wisuda, bucket bunga custom, dan bucket bunga murah di Jogja." />
-//         <meta property="og:image" content="/path-to-your-image.jpg" />
-//         <meta property="og:url" content="https://www.your-website.com" />
-//         <meta name="twitter:card" content="summary_large_image" />
-//         <link rel="icon" href="/favicon.ico" />
-//         <Script
-//           id="gtm-script"
-//           strategy="afterInteractive"
-//           dangerouslySetInnerHTML={{
-//             __html: `
-//               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-//               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-//               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-//               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-//               })(window,document,'script','dataLayer','GTM-TWN2GXHC');
-//             `,
-//           }}
-//         />
-//       </Head>
-//       <body>
-//         <Script
-//           strategy="afterInteractive"
-//           src="https://www.googletagmanager.com/gtag/js?id=G-B20N8LCNY3"
-//         />
-//         <Script id="ga-script" strategy="afterInteractive">
-//           {`
-//             window.dataLayer = window.dataLayer || [];
-//             function gtag(){dataLayer.push(arguments);}
-//             gtag('js', new Date());
-
-//             gtag('config', 'G-B20N8LCNY3');
-//           `}
-//         </Script>
-//         <noscript>
-//           <iframe
-//             src="https://www.googletagmanager.com/ns.html?id=GTM-TWN2GXHC"
-//             height="0"
-//             width="0"
-//             style={{ display: 'none', visibility: 'hidden' }}
-//           ></iframe>
-//         </noscript>
-//         {children}
-//       </body>
-//     </html>
-//   );
-// };
-
-// export default Layout;
 

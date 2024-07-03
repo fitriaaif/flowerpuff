ee495df262c5658dc787b4bd3ad1b3341c415089
Farhanmuh69 <fmuh748@gmail.com>
2024-07-03 10:44:15 +0700

    update

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl:'https://www.yoursite.com',
+  generateRobotsTxt: true, 
+};
+
+module.exports = config;
--- next-sitemap.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://flowerpuff.vercel.app', // Ganti dengan URL situs Anda
-  generateRobotsTxt: true,
-};
